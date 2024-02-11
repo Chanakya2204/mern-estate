f4c7b51524c3d26214ae6988ec5cbf5a02c304fb
Chanakya2204 <beherakumarbinay@gamil.com>
2024-02-11 22:34:31 +0530

    create signup and api route

--- api/index.js
+++ api/index.js
@@ -1,14 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
-import UserRouter from "./routes/user.route.js"
-
+import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 
 
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://mernestate2:8Y2bwAPld3ysTV1Y@mycluster2.svgjhex.mongodb.net/?retryWrites=true&w=majority",
+    await mongoose.connect("mongodb+srv://mongobinay:bTR8KWNJ6zdAT8sj@cluster01.vr82iyf.mongodb.net/?retryWrites=true&w=majority",
       { dbName: "real-estate" }
     );
     console.log("Connected to MongoDB!");
@@ -16,21 +15,21 @@ const connectToMongo = async () => {
     console.error("Failed to connect to MongoDB:", err);
   }
 };
-// mongoose
-// .connect(process.env.MONGO)
-// .then(()=>{
-//     console.log('connected to MOngoDb!');
-// })
-// .catch((err) => {
-//     console.log(err);
-// })
+
 
 const app = express();
 
+app.use(express.json())
+ 
 app.listen(3000, () => {
   connectToMongo();
   console.log("server is running on 3000!!! ");
 });
 
 //creating a api route
-app.use("/api/user",UserRouter);
+app.use("/api/user",userRouter);
+
+app.use("/api/auth",authRouter);
+
+
+
